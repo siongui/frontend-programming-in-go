495a3126290e91ff13660938ca72f083cf0e375f
siongui <siongui@gmail.com>
2018-01-18 08:42:11 +0800

    bulma navbar with js toggle

---
+++ 021-bulma-navbar/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Dropdowns
+
+  var $dropdowns = getAll('.navbar-item.has-dropdown:not(.is-hoverable)');
+
+  if ($dropdowns.length > 0) {
+    $dropdowns.forEach(function ($el) {
+      $el.addEventListener('click', function (event) {
+        event.stopPropagation();
+        $el.classList.toggle('is-active');
+      });
+    });
+
+    document.addEventListener('click', function (event) {
+      closeDropdowns();
+    });
+  }
+
+  function closeDropdowns() {
+    $dropdowns.forEach(function ($el) {
+      $el.classList.remove('is-active');
+    });
+  }
+
+  // Toggles
+
+  var $burgers = getAll('.burger');
+
+  if ($burgers.length > 0) {
+    $burgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+
+  // Functions
+
+  function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+  }
+});
